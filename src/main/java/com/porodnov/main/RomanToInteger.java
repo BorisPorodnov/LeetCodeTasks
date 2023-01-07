7f6d2fcd83733ceabeaadb431934e635ba437f61
Boris Porodnov <porodnov.b@mail.ru>
2023-01-07 16:30:17 +0700

    first realiasation task RomanToInt

---
+++ src/main/java/com/porodnov/main/RomanToInteger.java
@@ -0,0 +1,36 @@
+package com.porodnov.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    // initial
+    Map<Character, Integer> map = new HashMap<>();
+
+    {
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
+
+    public int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+        //обход обекта, c право на лево, с проверкой о том чему равен символ
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = map.get(s.charAt(i));
+            if (current < prev) {
+                result -= current;
+            } else {
+                result += current;
+            }
+            prev = current;
+        }
+        return result;
+    }
+}
