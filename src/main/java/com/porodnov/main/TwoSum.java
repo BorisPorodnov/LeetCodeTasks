905bf98f4874a9976ddc2fa06bc376497876852e
Boris Porodnov <porodnov.b@mail.ru>
2024-01-30 21:41:06 +0700

    commit

--- src/main/java/com/porodnov/main/TwoSum.java
+++ src/main/java/com/porodnov/main/TwoSum.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
+    public static void main(String[] args) {
+        twoSum(new int[] {9,2,7,8,}, 9);
+    }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         // создание хэш-таблицы для хранения чисел и их индексов
         Map<Integer, Integer> map = new HashMap<>();
         // перебор массива чисел
